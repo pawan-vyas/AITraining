2847c9f451766c24f39a0c63b1fe2d8ffa6ca4f6
Pawan Vyas <pawan.vyas.gitlab@gmail.com>
2025-04-11 16:57:46 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+var modelId = "gpt-4o-mini";
+var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY")
+             ?? throw new InvalidOperationException("Please set the OpenAI API key in the environment variable.");
+
+var kernelBuilder = Kernel.CreateBuilder()
+                          .AddOpenAIChatCompletion(modelId: modelId, apiKey: apiKey);
+
+var kernel = kernelBuilder.Build();
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+var promptExecutionSettings = new OpenAIPromptExecutionSettings()
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(),
+};
+
+var conversation = new ChatHistory();
+
+var exitKeys = new List<string> { "exit", "quit", "q" };
+var userInpt = string.Empty;
+do
+{
+    Console.Write("You > ");
+    userInpt = Console.ReadLine() ?? string.Empty;
+
+    if (exitKeys.Any(key => userInpt.Equals(key, StringComparison.OrdinalIgnoreCase)))
+        break;
+
+    conversation.AddUserMessage(userInpt);
+    var result = await chatCompletionService.GetChatMessageContentAsync(conversation, promptExecutionSettings, kernel);
+    Console.WriteLine($"AI > {result.Content}");
+    conversation.AddAssistantMessage(result.Content);
+} while (true);
+
+Console.WriteLine("Goodbye!");
